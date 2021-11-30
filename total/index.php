1071073fd7d287cd16c77e1ccd1be4d4719681db
el9998 <91722431+el9998@users.noreply.github.com>
2021-11-30 08:23:37 +0100

    Add files via upload

---
+++ total/index.php
@@ -0,0 +1,41 @@
+<?php include('./app/views/headers/html-header.php'); ?><html>
+<body class="bg-light">
+   <?php include('./app/views/headers/connect-header.php'); ?>
+    <div class="container">
+   <h1>Qui sommes-nous ?</h1>
+
+    <h2>TotalEnergies, une compagnie multi-énergies</h2>
+
+    <p  class="text-start">TotalEnergies est une compagnie multi-énergies mondiale de production et de fourniture d’énergies : pétrole et biocarburants, gaz naturel et gaz verts, renouvelables et électricité. 
+    Ses 105 000 collaborateurs s'engagent pour une énergie toujours plus abordable, propre, fiable et accessible au plus grand nombre. 
+    Présent dans plus de 130 pays, TotalEnergies inscrit le développement durable dans toutes ses dimensions au cœur de ses projets et opérations pour contribuer au bien-être des populations.</p>
+
+    <h1>Plus d'actualités</h1>
+    <div class="mb-3 container">
+
+    
+    <p><iframe  class="rounded float-end" width="560" height="315" src="https://www.youtube.com/embed/TTHHsEcQuUA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> Aujourd’hui, l’hydrogène est produit à 95 % à partir d’énergies fossiles et quasi exclusivement utilisé par l’industrie dans le raffinage et la chimie.
+    Demain, à condition qu’il soit décarboné, il jouera un rôle majeur dans la transition énergétique de l’industrie lourde, des transports, du gaz naturel et dans la performance des énergies renouvelables. C’est pourquoi TotalEnergies et ENGIE se sont associées pour développer le plus grand site de production d’hydrogène vert de France. Au cœur de la bioraffinerie de TotalEnergies à La Mède (France), le projet Masshylia sera alimenté par des fermes solaires.
+    L’électrolyseur qui y sera installé produira 5 tonnes d’hydrogène vert par jour à horizon 2024. Celui-ci servira à couvrir les besoins en hydrogène du processus de production de biocarburants de la raffinerie, évitant 15 000 tonnes d’émissions de CO2 par an.</p>    
+    </div>
+    <div class="container text-center">
+    <div class="field-title"><h2>Où nous trouver ?</h2>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2623.1865772568603!2d2.2411446154478214!3d48.89278130623896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47fa657a1827f5af%3A0x968cbd4e5b7000ea!2sTotal%20Tour%20Coupole!5e0!3m2!1sfr!2sfr!4v1634817830910!5m2!1sfr!2sfr" width="500" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+      <div class="field-title"><h2>Suivez-nous</h2>
+<ul>
+<a href="https://www.facebook.com/TotalEnergiesFrance" target="_blank"><img typeof="foaf:Image" src="https://services.totalenergies.fr/sites/g/files/wompnd2321/f/styles/picto/public/fb.png?itok=vlxJ4gRM" alt="Facebook de Total.fr"></a>
+<a href="https://www.youtube.com/channel/UCCQ5splvX84mIavSenAN4Ew" target="_blank"><img typeof="foaf:Image" src="https://services.totalenergies.fr/sites/g/files/wompnd2321/f/styles/picto/public/youtube.png?itok=yV1LLbG8" alt="Youtube de Total.fr"></a>
+<a href="https://twitter.com/TotalEnergiesFR" target="_blank"><img typeof="foaf:Image" src="https://services.totalenergies.fr/sites/g/files/wompnd2321/f/styles/picto/public/twitter_0.png?itok=sHEK6uxd" alt="Twitter de Total.fr"></a>
+<a href="https://www.linkedin.com/company/totalenergies/" target="_blank"><img typeof="foaf:Image" src="https://services.totalenergies.fr/sites/g/files/wompnd2321/f/styles/picto/public/linked_in.png?itok=_WKHlt4q" alt="LinkedIn de Total.fr"></a>
+<a href="https://www.instagram.com/totalenergies_fr/" target="_blank"><img typeof="foaf:Image" src="https://services.totalenergies.fr/sites/g/files/wompnd2321/f/styles/picto/public/instagram-total.fr_.png?itok=Wa4Ivzo2" alt="Instagram de Total.fr"></a>
+</ul>
+</div>
+  </div>
+  
+  
+
+</body>
+<footer  class="blockquote text-center"><p>TotalEnergies &copy; <?php echo date("Y"); ?></p></footer>
+</html>
+  
